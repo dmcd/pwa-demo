92c522270f3a4956d897a999e8d448b582ea9f30
David McDonald <davesaddress.nz@gmail.com>
2019-08-31 16:46:58 +1000

    Change to use postgres and re-enable signup

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_08_31_035513) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.boolean "done", default: false
