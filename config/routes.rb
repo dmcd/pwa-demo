d3810c0b66280f1feeae56a461a49851cbe25cef
David McDonald <davesaddress.nz@gmail.com>
2019-08-31 16:56:56 +1000

    Disable sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users 
-  # devise_for :users, :skip => [:registrations] 
-  # as :user do
-  #   get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
-  #   put 'users' => 'devise/registrations#update', :as => 'user_registration'
-  # end
+  # devise_for :users 
+  devise_for :users, :skip => [:registrations] 
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
 
   root to: "apps/todos#index"
 
