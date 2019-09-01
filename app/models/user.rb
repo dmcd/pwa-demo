9d7e5b156c423967b3cbd62552cebbec6f2206b8
David McDonald <davesaddress.nz@gmail.com>
2019-09-01 17:19:14 +1000

    Delete webpush subscription when deleting user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   acts_as_token_authenticatable
-  
+
+  has_one :webpush_subscription, dependent: :delete
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
